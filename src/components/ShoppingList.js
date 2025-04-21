e784893f345feca1d9b330a35a04e1030b6809bd
moreen karimi <moreenk541@gmail.com>
2025-04-21 11:16:08 +0300

    add items functionality

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,35 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search,setSearch] = useState("")
+  const [itemList, setItemList] =useState(items)
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  function handleSearchChange(e){
+    setSearch(e.target.value);
+  }
+  function handleAddItem(newItem){
+    setItemList([...itemList,newItem])
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  }
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = itemList.filter((item) => {
+    return (
+      (selectedCategory === "All" || item.category === selectedCategory) &&
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleAddItem} />
+      <Filter
+       onCategoryChange={handleCategoryChange} 
+       search={search}
+       onSearchChange={handleSearchChange}
+       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
